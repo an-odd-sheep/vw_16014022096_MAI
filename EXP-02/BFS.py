797b48b430d46305ce56fa810dfad62e67460b15
vrishank.w <vrishank.w@somaiya.edu>
2024-02-23 01:55:34 +0530

    dazai

--- EXP-02/BFS.py
+++ EXP-02/BFS.py
@@ -1,15 +1,19 @@
+counter = 0  
+
 class tree:
     def __init__(self, value):
         self.val = value
         self.children = []
 
 def bfs(root, search_value):
+    global counter  
     if root is None:
         return None
 
     queue = [root]
 
     while queue:
+        counter += 1  
         node = queue.pop(0)
         if search_value == node.val:
             return node.val
@@ -17,13 +21,14 @@ def bfs(root, search_value):
         for child in node.children:
             queue.append(child)
 
-
 root = tree(1)
 root.children = [tree(2), tree(3), tree(4)]
 root.children[0].children = [tree(5), tree(6)]
 root.children[2].children = [tree(7), tree(8)]
 
 print("Implementing BFS for search tree")
-search_value = int(input("Enter value you wanna search for: "))
+search_value = int(input("Enter value you want to search for: "))
 
-print("Node found:", bfs(root, search_value))
+x = bfs(root, search_value)
+print("Node found:", x)
+print("Time Complexity:", counter)
